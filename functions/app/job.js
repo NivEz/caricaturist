3f9b118709bab52f321d5d911d5ebfef43270633
NivEz <niv.cyber@gmail.com>
2022-09-03 14:00:23 +0300

    optimized cron job timing

--- functions/app/job.js
+++ functions/app/job.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions").region('europe-west1');
 const {main} = require('./main');
 
-exports.scheduledFunction = functions.pubsub.schedule('30 8 * * *')
+exports.scheduledFunction = functions.pubsub.schedule('30 8 * * 0-4')
     .timeZone('Asia/Jerusalem')
     .onRun((context) => {
         main();
